1ca63dd090768196eb57ebf71378a3eaded54415
GersonEC <gersonenriquez94@gmail.com>
2023-03-06 16:53:17 +0100

    Implements Inputs to search books

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
+const BOOKSHELF_BASE_URL = 'https://www.googleapis.com/books/v1/users';
+
+const fetchVolumes = async (term: string) => {
+  const res = await fetch(`${BASE_URL}?q=${term}`);
+  const json = await res.json();
+  return json;
+};
+
+const fetchVolume = async (id: string) => {
+  const res = await fetch(`${BASE_URL}/${id}`);
+  const json = await res.json();
+  return json;
+};
+
 function App() {
+  const [search, setSearch] = useState('');
+
+  const searchVolumes = async () => {
+    const books = await fetchVolumes(search);
+    console.log(books);
+    setSearch('');
+  };
+
+  //"Cx6aDwAAQBAJ"
+  const searchVolume = async () => {
+    const books = await fetchVolume(search);
+    console.log(books);
+    setSearch('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div>
+          <label htmlFor='volumesSearch'>Search Volumes</label>
+          <input
+            name='volumesSearch'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchVolumes}>Search</button>
+        </div>
+        <div>
+          <label htmlFor='volumeSearch'>Search specific Volume</label>
+          <input
+            name='volumeSearch'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchVolume}>Search</button>
+        </div>
       </header>
     </div>
   );
