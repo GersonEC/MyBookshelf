b9e6edb765acaceb7e10e33cf055645a033e9795
GersonEC <gersonenriquez94@gmail.com>
2023-03-14 13:10:45 +0100

    Remove useUsers hook

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import BookList from '../../components/BookList';
 import Button from '../../components/Button';
 import { UserContext } from '../../context/UserProvider';
+import useSignout from '../../hooks/useSignout';
 import useVolumes from '../../hooks/useVolumes';
 import { BooksWrapper } from './Home.styles';
 
@@ -16,7 +17,7 @@ const Home = () => {
   const [term, setTerm] = useState('');
   const mutation = useVolumes(term);
   const { user } = useContext(UserContext);
-
+  const signout = useSignout();
   const searchVolumes = async () => {
     mutation.mutate();
   };
@@ -33,7 +34,10 @@ const Home = () => {
   return (
     <main>
       <h1>Home</h1>
-      <p>User name: {user && user.name} </p>
+      <div>
+        <p>User name: {user && user.name} </p>
+        <Button onClick={() => signout()} label='Sign out' />
+      </div>
       <Button label='Ciao' onClick={() => alert('ciao')} />
       <Link to='/bookshelf'>Bookshelf</Link>
       <br />
@@ -47,7 +51,7 @@ const Home = () => {
           value={term}
           onChange={(e) => setTerm(e.target.value)}
         />
-        <button onClick={searchVolumes}>Search</button>
+        <Button onClick={searchVolumes} label='Search' />
       </div>
       {/* <div>
         <label htmlFor='volumeSearch'>Search specific Volume</label>
